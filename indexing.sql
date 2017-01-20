e12991ab6e8316fb6ba6dd8604b076fd3efeb754
Andrea <takamiya.andrea@gmail.com>
2017-01-19 19:35:15 -1000

    Completed Timing Section #1 and #2

--- indexing.sql
+++ indexing.sql
@@ -1,2 +1,8 @@
-CREATE USER "indexed_cars_user";
+-- CREATE USER "indexed_cars_user";
 CREATE DATABASE "indexed_cars" OWNER "indexed_cars_user";
+
+SELECT DISTINCT "make_title" FROM "car_models"
+  WHERE "make_code" = 'LAM';
+
+SELECT DISTINCT "model_title" FROM "car_models"
+  WHERE "make_code" = 'NISSAN' AND "model_code" = 'GT-R';
